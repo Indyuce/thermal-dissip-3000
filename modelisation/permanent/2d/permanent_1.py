0a09c0c36258b72e7da179a3dc1f16286f8d2367
Indyuce <cym.peyrat@yahoo.fr>
2020-04-15 17:03:32 +0200

    syst 1000 equations

--- modelisation/permanent/2d/permanent_1.py
+++ modelisation/permanent/2d/permanent_1.py
@@ -20,6 +20,54 @@ q = 100
 # coefficient de transfert par convection
 h = 10
 
+def findNature(x, y):
+    
+    # Coins
+    if x == 0 and y == 0:
+        return CoinInferieurGauche(x, y)
+    if x == size - 1 and y == 0:
+        return CoinInferieurDroit(x, y)
+    if x == 0 and y == size - 1:
+        return CoinSuperieurGauche(x, y)
+    if x == size - 1 and y == size - 1:
+        return CoinSuperieurDroit(x, y)
+    
+    # Surface
+    if x == 0:
+        return SurfaceVerticaleDroite(x, y)
+    if x == size - 1:
+        return SurfaceVerticaleGauche(x, y)
+    if y == 0:
+        return SurfaceHorizontaleHaut(x, y)
+    if y == size - 1:
+        return SurfaceHorizontaleBas(x, y)
+    
+    # Le reste, fort heureusement la majorité des blocs seront des
+    # équations très simples donc moins d'approximations aux flottants
+    return Centre(x, y)
+
+def solve():
+    
+    A = np.zeros((size**2, size**2))
+    B = np.zeros((size**2))
+    
+    for k in range(0, size**2):
+        
+        # coordonnées du point
+        x = k % size
+        y = k // size
+        
+        # nature du point
+        point = findNature(x, y)
+        
+        A[k] = point.getEquation()
+        B[k] = -q * dx**2 + point.getConvectionTerm()
+    
+    return np.linalg.solve(A, B)
+
+def getIndex(x, y):
+    return x + y * size
+
 class Point(ABC):
     def __init__(self, x, y):
         self.x = x
@@ -40,7 +88,7 @@ class Centre(Point):
         
     def getEquation(self):
         A = np.zeros(size**2)
-        index = size * self.x + self.y
+        index = getIndex(self.x, self.y)
         A[index] = -4 * k
         A[index - 1] = k
         A[index + 1] = k
@@ -48,7 +96,7 @@ class Centre(Point):
         A[index + size] = k
         return A
     
-    def getTerme(self):
+    def getConvectionTerm(self):
         return 0
 
 ## coins
@@ -64,9 +112,9 @@ class Coin(Point):
         
     def getEquation(self):
         A = np.zeros(size**2)
-        index = size * self.x + self.y
+        index = getIndex(self.x, self.y)
         A[index + self.cote] = k
-        A[index - size * self.hauteur] = k
+        A[index + size * self.hauteur] = k
         A[index] = -2 * k - 2 * h * dx
         return A
     
@@ -102,7 +150,7 @@ class Surface(Point):
     
     def getEquation(self):
         A = np.zeros(size**2)
-        index = size * self.x + self.y
+        index = getIndex(self.x, self.y)
         A[index + self.i1] = k
         A[index + self.i2] = k
         A[index + self.i3] = k
@@ -112,19 +160,19 @@ class Surface(Point):
     def getConvectionTerm(self):
         return -h * dx * Tinf
 
-class SurfaceVerticaleHaut(Surface):
-    def __init__(self, x, y):
-        super().__init__(x, y, -1, 1, -size)
-
-class SurfaceVerticaleBas(Surface):
+class SurfaceHorizontaleHaut(Surface):
     def __init__(self, x, y):
         super().__init__(x, y, -1, 1, size)
 
-class SurfaceHorizontaleDroite(Surface):
+class SurfaceHorizontaleBas(Surface):
+    def __init__(self, x, y):
+        super().__init__(x, y, -1, 1, -size)
+
+class SurfaceVerticaleDroite(Surface):
     def __init__(self, x, y):
         super().__init__(x, y, size, -size, 1)
 
-class SurfaceHorizontaleGauche(Surface):
+class SurfaceVerticaleGauche(Surface):
     def __init__(self, x, y):
         super().__init__(x, y, size, -size, -1)
 
