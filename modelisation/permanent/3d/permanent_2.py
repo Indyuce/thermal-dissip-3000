04f31d2e91f23ab51129589b0a52f0d45cb760ee
Indyuce <cym.peyrat@yahoo.fr>
2020-04-15 19:09:12 +0200

    Ajout poo

--- modelisation/permanent/3d/permanent_2.py
+++ modelisation/permanent/3d/permanent_2.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from abc import ABC, abstractmethod
 import time
-import scipy as sc 
 size = 100
 
 # température avec échange par convection avec l'extérieur
@@ -21,32 +19,6 @@ q = 100
 # coefficient de transfert par convection
 h = 10
 
-def findNature(x, y, z):
-    
-    # Coins
-    if x == 0 and y == 0:
-        return CoinInferieurGauche(x, y)
-    if x == size - 1 and y == 0:
-        return CoinInferieurDroit(x, y)
-    if x == 0 and y == size - 1:
-        return CoinSuperieurGauche(x, y)
-    if x == size - 1 and y == size - 1:
-        return CoinSuperieurDroit(x, y)
-    
-    # Surface
-    if x == 0:
-        return SurfaceVerticaleDroite(x, y)
-    if x == size - 1:
-        return SurfaceVerticaleGauche(x, y)
-    if y == 0:
-        return SurfaceHorizontaleHaut(x, y)
-    if y == size - 1:
-        return SurfaceHorizontaleBas(x, y)
-    
-    # Le reste, fort heureusement la majorité des blocs seront des
-    # équations très simples donc moins d'approximations aux flottants
-    return Centre(x, y)
-
 def solve():
     t1=time.perf_counter()
     ax=Axes3D(plt.figure())
@@ -60,10 +32,10 @@ def solve():
         y = k // size
         
         # nature du point
-        point = findNature(x, y)
+        # point = findNature(x, y)
         
-        A[k] = point.getEquation()
-        B[k] = -q * dx**2 + point.getConvectionTerm()
+        # A[k] = point.getEquation()
+        # B[k] = -q * dx**2 + point.getConvectionTerm()
     
     C=np.linalg.solve(A, B)
     print(C)
@@ -77,10 +49,12 @@ def solve():
     print(t2-t1)
     plt.show()
     
-def getIndex(x, y):
+# donne le numero de coefficient dans l'équation linéaire, en fonction de x, y, et z
+# x, y, z concaténés représentent l'écriture de getIndex(x, y, z) en base 'size'
+def getIndex(x, y, z):
     return x + y * size + z * size**2
 
-class Point(ABC):
+class Point:
     def __init__(self, x, y, z, c):
         
         # coordonées du point
@@ -88,121 +62,47 @@ class Point(ABC):
         self.y = y
         self.z = z
         
-        # contacts avec les blocs de métal autour
+        # contacts avec les blocs élémentaires adjacents
+        # liste d'offsets, len(c) = nb de contacts
+        # (6 - len(c)) = nb de contacts convectifs
         self.c = c
     
     def getEquation(self):
         A = np.zeros(size**3)
         index = getIndex(self.x, self.y, self.z)
         
-        # appliquer les coefficients dans l'équation linéaire
+        # la température du bloc intervient autant de fois dans l'équation linéaire
+        # qu'il y a de contacts avec les blocs adjacents, d'où len(c) * k
         
+        # (6 - len(c)) * h * dc représente le fait que la temp du bloc intervient
+        # dans le delta de température dans le calcul du flux thermique convectif,
+        # d'où (6 - len(c)) * h * dx
+        A[index] = -len(self.c) * k - (6 - len(self.c)) * h * dx
         
-        
-        A[index] = -4 * k
-        A[index - 1] = k
-        A[index + 1] = k
-        A[index - size] = k
-        A[index + size] = k
+        # appliquer les coefficients dans l'équation linéaire car les températures des
+        # blocs adjacents interviennent avec un facteur k dans le transfert par conduction
+        for a in self.c:
+            A[index + a] = k
     
-    # terme de convection à droite de l'égalité
+    # terme de convection à droite de l'égalité, il ne faut pas oublier
+    # de lui sommer le terme de puissance volumique.
     def getConvectionTerm(self):
-        return - (6 - len(c)) * h * dx * Tinf
+        return - (6 - len(self.c)) * h * dx * Tinf
 
+# Un centre est défini par aucun contact convectif
+# tous les contacts sont conductifs et il n'y a aucune ambiguité sur l'orientation des contacts
 class Centre(Point):
+    
+    # Aucun contact convectif
     def __init__(self, x, y, z):
-        super().__init__(x, y, z, [])
-        
-    def getEquation(self):
-        A = np.zeros(size**2)
-        index = getIndex(self.x, self.y)
-        A[index] = -4 * k
-        A[index - 1] = k
-        A[index + 1] = k
-        A[index - size] = k
-        A[index + size] = k
-        return A
-    
-    def getConvectionTerm(self):
-        return 0
+        super().__init__(x, y, z, [-1, 1, size, -size, size**2, -size**2])
+
+# Un "coin" possède 3 contacts convectifs
+# Un "bord" possède 2 contacts convectifs
+# Une "surface" possède 3 contacts convectifs
+# Une "lame" possède 4 contacts convectifs
 
 
-
-## coins
-
-# coté et hauteur de métal en contact
-# côté = 1 pour gauche, -1 pour droite
-# hauteur = 1 pour supérieur, -1 pour inférieur
-class Coin(Point):
-    def __init__(self, x, y, cote, hauteur):
-        super().__init__(x, y)
-        self.cote = cote
-        self.hauteur = hauteur
-        
-    def getEquation(self):
-        A = np.zeros(size**2)
-        index = getIndex(self.x, self.y)
-        A[index + self.cote] = k
-        A[index + size * self.hauteur] = k
-        A[index] = -2 * k - 2 * h * dx
-        return A
-    
-    def getConvectionTerm(self):
-        return -2 * h * dx * Tinf
-
-class CoinInferieurGauche(Coin):
-    def __init__(self, x, y):
-        super().__init__(x, y, 1, 1)
-
-class CoinInferieurDroit(Coin):
-    def __init__(self, x, y):
-        super().__init__(x, y, -1, 1)
-
-class CoinSuperieurGauche(Coin):
-    def __init__(self, x, y):
-        super().__init__(x, y, 1, -1)
-
-class CoinSuperieurDroit(Coin):
-    def __init__(self, x, y):
-        super().__init__(x, y, -1, -1)
-
-## surfaces
-
-# surface
-# i1, i2 et i3 sont les index des blocs en contact avec le bloc élem
-class Surface(Point):
-    def __init__(self, x, y, i1, i2, i3):
-        super().__init__(x, y)
-        self.i1 = i1
-        self.i2 = i2
-        self.i3 = i3
-    
-    def getEquation(self):
-        A = np.zeros(size**2)
-        index = getIndex(self.x, self.y)
-        A[index + self.i1] = k
-        A[index + self.i2] = k
-        A[index + self.i3] = k
-        A[index] = -3 * k - h * dx
-        return A
-    
-    def getConvectionTerm(self):
-        return -h * dx * Tinf
-
-class SurfaceHorizontaleHaut(Surface):
-    def __init__(self, x, y):
-        super().__init__(x, y, -1, 1, size)
-
-class SurfaceHorizontaleBas(Surface):
-    def __init__(self, x, y):
-        super().__init__(x, y, -1, 1, -size)
-
-class SurfaceVerticaleDroite(Surface):
-    def __init__(self, x, y):
-        super().__init__(x, y, size, -size, 1)
-
-class SurfaceVerticaleGauche(Surface):
-    def __init__(self, x, y):
-        super().__init__(x, y, size, -size, -1)
-
-       
+# Un programme tiers codé sur Minecraft permet d'analyser une construction, et de former
+# une array de chiffres qui correspondent à des types de points, qui sont ensuite lus
+# par le logiciel Python pour former la géométrie
